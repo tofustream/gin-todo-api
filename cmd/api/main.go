746659405a6b6b5d732a7b02197d8658379df611
tofustream <177463621+tofustream@users.noreply.github.com>
2024-12-31 15:04:32 +0900

    Normalize line endings

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	log.Println("check")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		log.Println(err)
+	}
 }
